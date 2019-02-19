86e97517c52baa1c0b44a94bdfe7529abc96afdc
brujorge <brujorgegonzales@gmail.com>
2019-02-19 12:53:48 -0500

    Refactor map logic

--- main.js
+++ main.js
@@ -1,107 +1,104 @@
-const width = 960;
-const height = 500;
+document.addEventListener("DOMContentLoaded", () => {
+  const MAP_WIDTH = 960;
+  const MAP_HEIGHT = 500;
 
-const projection = d3.geo
-  .albersUsa()
-  .translate([width / 2, height / 2])
-  .scale([1000]);
+  const projection = d3.geo
+    .albersUsa()
+    .translate([MAP_WIDTH / 2, MAP_HEIGHT / 2])
+    .scale([1000]);
 
-const path = d3.geo.path().projection(projection);
+  const path = d3.geo.path().projection(projection);
 
-const color = d3.scale
-  .linear()
-  .range(["#b0e8e0", "#00af94", "#00b89e", "#29c7b5", "#91dfd5"]);
+  const svg = d3
+    .select("#map")
+    .append("svg")
+    .attr("width", MAP_WIDTH)
+    .attr("height", MAP_HEIGHT);
 
-const svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height);
+  const marker = d3
+    .select("#map")
+    .append("div")
+    .attr("class", "map-tooltip")
+    .style("opacity", 0);
 
-const div = d3
-  .select("body")
-  .append("div")
-  .attr("class", "map-tooltip")
-  .style("opacity", 0);
+  const getMaxValueOfLawyers = statesGeoJson =>
+    Math.max(
+      ...statesGeoJson.features.map(state =>
+        state.properties.lawyers ? state.properties.lawyers : 0
+      )
+    );
 
-d3.csv("lawyers.csv", function(data) {
-  const maxValueOfLawyers = Math.max(...data.map(state => state.lawyers), 0);
-  const minValueOfLawyers = Math.min(...data.map(state => state.lawyers), 0);
-
-  color.domain([minValueOfLawyers, maxValueOfLawyers]);
-
-  d3.json("us-states.json", function(json) {
-    for (let i = 0; i < data.length; i++) {
-      const { state, lawyers } = data[i];
-
-      for (let j = 0; j < json.features.length; j++) {
-        const jsonState = json.features[j].properties.name;
-
-        if (state == jsonState) {
-          json.features[j].properties.lawyers = lawyers;
-          break;
-        }
-      }
-    }
+  const addStatesPath = (statesGeoJson, colorScale) => {
     svg
       .selectAll("path")
-      .data(json.features)
+      .data(statesGeoJson.features)
       .enter()
       .append("path")
       .attr("d", path)
-      .attr("id", function(d) {
-        return d.properties.name;
-      })
+      .attr("id", d => d.properties.name)
       .style("stroke", "#fff")
       .style("stroke-width", "1")
-      .style("fill", function(d) {
-        const value = d.properties.lawyers;
+      .style("fill", d =>
+        d.properties.lawyers ? colorScale(d.properties.lawyers) : "#b0e8e0"
+      );
+  };
+  d3.json("us-states.json", statesGeoJson => {
+    const colorScale = d3.scale
+      .linear()
+      .range(["#b0e8e0", "#00af94", "#00b89e", "#29c7b5", "#91dfd5"])
+      .domain([0, getMaxValueOfLawyers(statesGeoJson)]);
 
-        if (value) {
-          return color(value);
-        }
+    addStatesPath(statesGeoJson, colorScale);
 
-        return "#b0e8e0";
-      });
+    const getRandomState = () =>
+      statesGeoJson.features[
+        Math.floor(Math.random() * statesGeoJson.features.length)
+      ].properties;
 
-    function randomlyShowTooltip() {
-      setTimeout(function() {
-        const randomState = getRandomState();
-        showTooltip(randomState);
-
-        randomlyShowTooltip();
+    const initTooltipToggling = () => {
+      setTimeout(() => {
+        showTooltip(getRandomState());
+        initTooltipToggling();
       }, 2000);
-    }
+    };
 
-    function showTooltip({ name, lawyers }) {
-      const { x, y } = d3
+    const showTooltip = ({ name, lawyers }) => {
+      const getStateCenterPosition = stateName => {
+        const position = d3
+          .select(`path[id='${stateName}']`)
+          .node()
+          .getBBox();
 
-        .select(`path[id='${name}']`)
-        .node()
-        .getBBox();
-      div
-        .transition()
-        .duration(300)
-        .style("opacity", 1);
-      div
-        .html(
-          `<p class="map-tooltip-lawyers">${lawyers}</p> <p class="map-tooltip-state">${name}</p>`
-        )
-        .style("left", x + "px")
-        .style("top", (y - 10) + "px");
+        return position;
+      };
 
-      setTimeout(function hideTooltip() {
-        div
+      const fadeInTooltip = (x, y) => {
+        marker
+          .transition()
+          .duration(300)
+          .style("opacity", 1);
+
+        marker
+          .html(
+            `<p class="map-tooltip-lawyers">${lawyers}</p> <p class="map-tooltip-state">${name}</p>`
+          )
+          .style("left", x + "px")
+          .style("top", y - 10 + "px");
+      };
+
+      const { x, y } = getStateCenterPosition(name);
+
+      fadeInTooltip(x, y);
+
+      const fadeOutTooltip = () => {
+        marker
           .transition()
           .duration(500)
           .style("opacity", 0);
-      }, 1500);
-    }
+      }
+      setTimeout(fadeOutTooltip, 1500);
+    };
 
-    function getRandomState() {
-      return json.features[Math.floor(Math.random() * json.features.length)].properties
-    }
-
-    randomlyShowTooltip();
+    initTooltipToggling();
   });
 });
